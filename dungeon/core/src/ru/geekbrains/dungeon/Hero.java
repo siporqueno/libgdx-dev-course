01cbc43be86771ff86768a910c1d9fc8bb99b9e2
siporqueno <strike1000001@gmail.com>
2020-11-12 14:03:06 +0300

    Task 2 of homework of Lesson 2

--- dungeon/core/src/ru/geekbrains/dungeon/Hero.java
+++ dungeon/core/src/ru/geekbrains/dungeon/Hero.java
@@ -10,17 +10,25 @@ import com.badlogic.gdx.math.Vector2;
 
 public class Hero {
     private ProjectileController projectileController;
+    private int width;
+    private int height;
     private Vector2 position;
+    private float velocity;
+    private float angle;
     private TextureRegion texture;
     private boolean doubleShot;
 
     public Hero(TextureAtlas atlas, ProjectileController projectileController) {
         this.position = new Vector2(100, 100);
+        this.velocity = 150.0f;
         this.texture = atlas.findRegion("tank");
         this.projectileController = projectileController;
+        this.width=texture.getRegionWidth();
+        this.height=texture.getRegionHeight();
     }
 
     public void update(float dt) {
+        checkMovement(dt);
         if (Gdx.input.isKeyJustPressed(Input.Keys.Q)) doubleShot = !doubleShot;
         if (Gdx.input.isKeyJustPressed(Input.Keys.SPACE)) {
             projectileController.activate(position.x, position.y, 200, 0);
@@ -28,7 +36,28 @@ public class Hero {
         }
     }
 
+    public void checkMovement(float dt) {
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+            position.x -= velocity * dt;
+            angle = 180.0f;
+        } else if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+            position.x += velocity * dt;
+            angle = 0.0f;
+        } else if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
+            position.y += velocity * dt;
+            angle = 90.0f;
+        } else if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+            position.y -= velocity * dt;
+            angle = 270.0f;
+        }
+
+        if (position.x < width/2.0f) position.x = width/2.0f;
+        if (position.x > GameMap.CELLS_X*40-width/2.0f) position.x = GameMap.CELLS_X*40-width/2.0f;
+        if (position.y < 0.0f+height/2.0f) position.y = 0.0f+height/2.0f;
+        if (position.y > Gdx.graphics.getHeight()-height/2.0f) position.y = Gdx.graphics.getHeight()-height/2.0f;
+    }
+
     public void render(SpriteBatch batch) {
-        batch.draw(texture, position.x - 20, position.y - 20);
+        batch.draw(texture, position.x - width/2.0f, position.y - height/2.0f, width/2.0f, height/2.0f, width, height, 1, 1, angle);
     }
 }
